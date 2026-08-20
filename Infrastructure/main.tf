1d9462121329e6efb52c8025ba9135cf291ab7f6
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-20 21:35:38 +0100

    updated main.tf

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,3 +1,23 @@
 module "vpc" {
   source          = "./modules/vpc"
 }
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+}
+
+module "alb" {
+  source = "./modules/alb"
+}
+
+module "acm" {
+  source = "./modules/acm"
+}
+
+module "route53" {
+  source = "./modules/route53"
+}
