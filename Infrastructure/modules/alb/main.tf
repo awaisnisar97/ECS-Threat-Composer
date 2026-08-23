0990c6f6f8c326a4c4a111ae7d3f387ee8e4315f
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-23 11:20:05 +0100

    updated main.tf comments

--- Infrastructure/modules/alb/main.tf
+++ Infrastructure/modules/alb/main.tf
@@ -1,3 +1,4 @@
+# creating an application load balancer with the specified name, security group, and public subnets
 resource "aws_lb" "alb" {
   name               = var.alb_name
   internal           = false
@@ -7,14 +8,15 @@ resource "aws_lb" "alb" {
 
   enable_deletion_protection = true
 
+# enabling access logs for the ALB and specifying the S3 bucket and prefix for storing the logs
   access_logs {
-    bucket  = var.alb_access_log_bucket_name
-    prefix  = "test-lb"
-    enabled = true
+  bucket  = var.alb_access_log_bucket_name
+  prefix  = "test-lb"
+  enabled = true
   }
 }
 
-
+# creating a target group for the ALB with the specified name, port, protocol, and VPC ID
 resource "aws_lb_target_group" "alb_tg" {
   name        = var.alb_target_group_name
   port        = var.alb_target_group_port
@@ -31,7 +33,7 @@ resource "aws_lb_target_group" "alb_tg" {
   }
 }
 
-# 
+# creating a listener for the ALB to handle HTTP traffic
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
@@ -40,6 +42,7 @@ resource "aws_lb_listener" "http_listener" {
   default_action {
     type = "redirect"
 
+# redirecting HTTP traffic to HTTPS with a 301 status code
     redirect {
       port        = "443"
       protocol    = "HTTPS"
@@ -47,6 +50,8 @@ resource "aws_lb_listener" "http_listener" {
     }
   }
 }
+
+# creating a listener for the ALB to handle HTTPS traffic with the specified SSL certificate
 resource "aws_lb_listener" "https_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 443
@@ -61,14 +66,14 @@ resource "aws_lb_listener" "https_listener" {
   }
 }
 
-
-
+# creating a security group for the ALB with the specified name, description, and VPC ID
 resource "aws_security_group" "alb_sg" {
   name        = var.alb_security_group_name
   description = "Security group for the ALB"
   vpc_id      = var.vpc_id
 }
 
+# creating an ingress rule for the ALB security group to allow inbound HTTP traffic on port 80 from any IP address
 resource "aws_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.alb_sg.id
   from_port         = 80
@@ -77,6 +82,7 @@ resource "aws_security_group_ingress_rule" "allow_http" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
+# creating an ingress rule for the ALB security group to allow inbound HTTPS traffic on port 443 from any IP address
 resource "aws_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.alb_sg.id
   from_port         = 443
@@ -85,6 +91,7 @@ resource "aws_security_group_ingress_rule" "allow_https" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
+# creating an egress rule for the ALB security group to allow all outbound traffic to any IP address
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.alb_sg.id
   ip_protocol       = "-1"
