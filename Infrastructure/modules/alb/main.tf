22a8dc0b0215884addfa3ac2233239d2c2ffda91
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-23 11:02:19 +0100

    updated main.tf

---
+++ Infrastructure/modules/alb/main.tf
@@ -0,0 +1,91 @@
+resource "aws_lb" "alb" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = var.alb_public_subnet_ids
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = var.aws_s3_bucket.lb_logs.id
+    prefix  = "test-lb"
+    enabled = true
+  }
+}
+
+resource "aws_lb_target_group" "alb_tg" {
+  name        = var.alb_target_group_name
+  port        = var.alb_target_group_port
+  protocol    = var.alb_target_group_protocol
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    path                = "/health"
+  }
+}
+
+# 
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn = var.alb_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+
+
+resource "aws_security_group" "alb_sg" {
+  name        = var.alb_security_group_name
+  description = "Security group for the ALB"
+  vpc_id      = var.vpc_id
+}
+
+resource "aws_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.alb_sg.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.alb_sg.id
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.alb_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
