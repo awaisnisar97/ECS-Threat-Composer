9df4585a4426017d3169cd3c68cfd28511d32ec3
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-24 14:00:30 +0100

    updated route53 module

---
+++ Infrastructure/modules/route53/main.tf
@@ -0,0 +1,20 @@
+# look up the existing public route 53 hosted zone for the domain
+
+data "aws_route53_zone" "selected" {
+    name         = var.route53_hosted_zone_name
+    private_zone = false
+} 
+
+# create a Route 53 record for the ALB
+
+resource "aws_route53_record" "alb" {
+    zone_id = data.aws_route53_zone.selected.zone_id
+    name    = var.route53_record_name
+    type    = "A"
+
+    alias {
+        name                   = var.alb_dns_name
+        zone_id                = var.alb_zone_id
+        evaluate_target_health = true
+    }
+}
