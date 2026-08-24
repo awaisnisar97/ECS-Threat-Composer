cb60513439635ef9056dd48b08dc0e5967a1465a
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-24 16:17:45 +0100

    updated ecs main.tf added comment

--- Infrastructure/modules/ecs/main.tf
+++ Infrastructure/modules/ecs/main.tf
@@ -123,6 +123,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
   ip_protocol                  = "tcp"
 }
 
+# allow all outbound traffic from the ECS service security group to any destination
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.ecs_service_sg.id
   ip_protocol       = "-1"
