42d46e5fd6b35015ffd24b11cc0d75686b2180e9
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-24 14:01:30 +0100

    updated alb outputs.tf  module

--- Infrastructure/modules/alb/output.tf
+++ Infrastructure/modules/alb/output.tf
@@ -5,4 +5,11 @@ output "target_group_arn" {
  output "alb_security_group_id" {
    value = aws_security_group.alb_sg.id
  }
- 
+
+ output "alb_dns_name" {
+   value = aws_lb.alb.dns_name
+ }
+
+ output "alb_zone_id" {
+   value = aws_lb.alb.zone_id
+ }
