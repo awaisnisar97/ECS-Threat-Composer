bcf8739191f2623b4f3aaf6de1eaccce1302c275
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-24 14:03:27 +0100

    updated m odule

--- Infrastructure/modules/alb/output.tf
+++ Infrastructure/modules/alb/output.tf
@@ -1,15 +1,15 @@
 output "target_group_arn" {
-   value = aws_lb_target_group.app_tg.arn
- }
+  value = aws_lb_target_group.app_tg.arn
+}
 
- output "alb_security_group_id" {
-   value = aws_security_group.alb_sg.id
- }
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
 
- output "alb_dns_name" {
-   value = aws_lb.alb.dns_name
- }
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
 
- output "alb_zone_id" {
-   value = aws_lb.alb.zone_id
- }
+output "alb_zone_id" {
+  value = aws_lb.alb.zone_id
+}
