20c87e0862cf19d729f7fbe049a886f584cf6d19
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-25 10:50:42 +0100

    updated main.tf with variables

--- Infrastructure/modules/ecs/main.tf
+++ Infrastructure/modules/ecs/main.tf
@@ -127,5 +127,5 @@ resource "aws_vpc_security_group_ingress_rule" "allow_alb" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.ecs_service_sg.id
   ip_protocol       = "-1"
-  cidr_ipv4       = ["0.0.0.0/0"]
+  cidr_ipv4         = ["0.0.0.0/0"]
 }
