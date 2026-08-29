a59908c36c547542da4d45f014ebbcf741afef25
awaisnisar97 <139353205+awaisnisar97@users.noreply.github.com>
2026-08-29 15:59:13 +0100

    update root main.tf variables

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -24,24 +24,30 @@ module "ecr" {
 module "ecs" {
   source = "./modules/ecs"
 
-  ecs_cluster_name          = var.ecs_cluster_name
-  ecs_task_cpu              = var.ecs_task_cpu
-  ecs_task_memory           = var.ecs_task_memory
-  ecr_repository_url        = module.ecr.ecr_repository_url
-  private_subnet_ids         = [module.vpc.private_subnet1_id, module.vpc.private_subnet2_id]
-  ecs_service_desired_count = var.ecs_service_desired_count
-  ecs_container_name        = var.ecs_container_name
-  ecs_container_port        = var.ecs_container_port
-  target_group_arn          = module.alb.target_group_arn
-  aws_region                = var.aws_region
-  ecs_task_family           = var.ecs_task_family
+  load_balancer_sg_id          = module.alb.alb_security_group_id
+  ecs_service_name             = var.ecs_service_name
+  vpc_id                       = module.vpc.vpc_id
+  ecs_task_execution_role_name = var.ecs.ecs_task_execution_role_name
+  ecr_image_tag                = var.ecr_image_tag
+  ecs_cluster_name             = var.ecs_cluster_name
+  ecs_task_cpu                 = var.ecs_task_cpu
+  ecs_task_memory              = var.ecs_task_memory
+  ecr_repository_url           = module.ecr.ecr_repository_url
+  private_subnet_ids           = [module.vpc.private_subnet1_id, module.vpc.private_subnet2_id]
+  ecs_service_desired_count    = var.ecs_service_desired_count
+  ecs_container_name           = var.ecs_container_name
+  ecs_container_port           = var.ecs_container_port
+  target_group_arn             = module.alb.target_group_arn
+  aws_region                   = var.aws_region
+  ecs_task_family              = var.ecs_task_family
 }
 
 module "alb" {
   source = "./modules/alb"
 
+  alb_name                   = var.alb_name
   alb_access_log_bucket_name = var.alb_access_log_bucket_name
-  public_subnet_ids           = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
+  public_subnet_ids          = [module.vpc.public_subnet1_id, module.vpc.public_subnet2_id]
   alb_target_group_name      = var.alb_target_group_name
   alb_target_group_port      = var.alb_target_group_port
   alb_target_group_protocol  = var.alb_target_group_protocol
@@ -63,4 +69,5 @@ module "route53" {
   route53_hosted_zone_name = var.route53_hosted_zone_name
   alb_dns_name             = module.alb.alb_dns_name
   alb_zone_id              = module.alb.alb_zone_id
+  route53_record_name      = var.route53_record_name
 }
